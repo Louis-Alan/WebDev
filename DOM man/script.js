b3be612a2759f8a6fcd3d31fc53a2333040ff165
Alan Louis <alouis811@gmail.com>
2022-09-06 16:01:21 +0530

    Button DOM

--- DOM man/script.js
+++ DOM man/script.js
@@ -19,3 +19,25 @@ div2.style.cssText = "border: 2px solid black; background-color: hotpink";
 div2.innerHTML += `<h1>I'm in a div</h1>
     <p> ME TOO!</p>`;
 container.appendChild(div2);
+
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert("HIHIHI");
+
+let btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+//     alert("Function alert");
+// })
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
+
+let buttons = document.querySelectorAll('#potato button');
+
+console.log(buttons);
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
